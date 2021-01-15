94c5c9d2934ab2e03319d74a446080e35074354f
albeertito7 <albertperezdatsira@gmail.com>
2021-01-15 16:49:31 +0100

    aStar search done

--- py3/search/search.py
+++ py3/search/search.py
@@ -166,7 +166,28 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    ## util.raiseNotDefined()
+    startingNode = problem.getStartState()
+
+    pQueue = util.PriorityQueue()
+    visitedNodes = []
+    pQueue.push((startingNode, [], 0), 0)
+
+    while not pQueue.isEmpty():
+
+        currentNode, actions, prevCost = pQueue.pop()
+        
+        if problem.isGoalState(currentNode):
+                return actions
+
+        if currentNode not in visitedNodes:
+            visitedNodes.append(currentNode)
+
+            for nextNode, action, cost in problem.getSuccessors(currentNode):
+                newAction = actions + [action]
+                newCostToNode = prevCost + cost
+                heuristicCost = newCostToNode + heuristic(nextNode,problem)
+                pQueue.push((nextNode, newAction, newCostToNode),heuristicCost)
 
 
 # Abbreviations
